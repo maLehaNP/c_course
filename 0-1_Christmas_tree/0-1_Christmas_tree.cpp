b68aee7d077dbee69fb8835897646dd7f5132392
maLeha <arapov-5@mail.ru>
2025-10-29 17:40:05 +0400

    Done 0.1

---
+++ 0-1_Christmas_tree/0-1_Christmas_tree.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "|   Happy   | \n";
+    cout << "|-----------| \n";
+    cout << "|     *     | \n";
+    cout << "|    / \\o   | \n";
+    cout << "|  o// \\\\   | \n";
+    cout << "|  /// \\o\\  | \n";
+    cout << "|    | |    | \n";
+    cout << "|-----------| \n";
+    cout << "| New Year  | \n";
+}
