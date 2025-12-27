27dba2ff8e6b07172771d2e724103863c5882999
maLeha <maLeha@DESKTOP-GPB8UP5>
2025-12-27 14:48:35 +0400

    Done 4.1.

--- 4.1_Миникласс/4.1_Миникласс.cpp
+++ 4.1_Миникласс/4.1_Миникласс.cpp
@@ -1,12 +1,16 @@
 ﻿#include <iostream>
+using namespace std;
 
 class Container {
     int volume;
 public:
     // По умолчанию
-    Container() = default;
+    Container() {
+        cout << "Default constructor" << endl;
+    }
 
     Container(int volume) {
+        cout << "Normal constructor" << endl;
         if (volume < 0) {
             this->volume = 0;
         }
@@ -14,14 +18,31 @@ public:
     }
 
     // Копирования
-    Container(const Container& c) {
-        volume = c.volume;
+    Container(const Container& c) : Container{ c.getVolume() } {
+        cout << "Copy constructor" << endl;
+    }
+
+    // Перемещения
+    Container(Container&& moved) {
+        cout << "Move constructor" << endl;
+        volume = moved.volume;
+        moved.volume = 0;
+    }
+
+    // Оператор перемещения (move assignment operator)
+    Container& operator=(Container&& moved) {
+        cout << "Move assignment operator" << endl;
+        if (&moved != this) {  // Проверка на самоприсваивание
+            volume = moved.volume;
+            moved.volume = 0;
+        }
+        return *this;
     }
 
     // Деструктор
-    ~Container() = delete;
+    ~Container() = default;
 
-    int getVolume() {
+    int getVolume() const {
         return volume;
     }
 };
@@ -37,5 +58,38 @@ int main()
      * В дальнейшем можно только посмотреть значение объёма методом getVolume.
      */
     
+    // Default
+    Container c0;
+    cout << endl;
 
+    // Normal
+    Container c1(5);
+    cout << endl;
+
+    // < 0
+    Container c2(-5);
+    cout << "C2 volume: " << c2.getVolume() << endl;
+    cout << endl;
+
+    // Copy
+    Container c3(c1);
+    cout << "C3 volume: " << c3.getVolume() << endl;
+    cout << endl;
+
+    // Move
+    Container c4(move(c1));
+    cout << "C4 volume: " << c4.getVolume() << endl;
+    cout << "C1 volume: " << c1.getVolume() << endl;
+    cout << endl;
+
+    // Copy assign
+    Container c5(6);
+    Container c6 = c5;
+    cout << "C6 volume: " << c6.getVolume() << endl;
+    cout << endl;
+
+    // Move assign
+    c5 = Container{7};
+    cout << "C5 volume: " << c5.getVolume() << endl;
+    cout << endl;
 }
